441abec38a2c14f14cccbef126cb5057f4ae9c92
AnuragMurti28 <anuragmurti28@gmail.com>
2021-06-26 22:16:43 +0530

    updated home to homepage

--- tute67/index.js
+++ tute67/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const home = fs.readFileSync('./index.html');
+const homepage = fs.readFileSync('./index.html');
 const about = fs.readFileSync('./about.html');
 const services = fs.readFileSync('./services.html');
 const contact = fs.readFileSync('./contact.html');
@@ -14,7 +14,7 @@ const home =
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end(home);
+        res.end(homepage);
     });
 
 server.listen(port, hostname, () => {
