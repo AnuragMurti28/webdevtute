b5a4002dbe2a3cb1970d4e5388981f29a1b2f996
AnuragMurti28 <anuragmurti28@gmail.com>
2021-06-26 22:21:09 +0530

    no change

--- tute67/index.js
+++ tute67/index.js
@@ -2,20 +2,17 @@ const http = require('http');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3500;
 const homepage = fs.readFileSync('./index.html');
 const about = fs.readFileSync('./about.html');
 const services = fs.readFileSync('./services.html');
 const contact = fs.readFileSync('./contact.html');
 
-
-const home =
-
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(homepage);
-    });
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(homepage);
+});
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
