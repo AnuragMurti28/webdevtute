8cb04716795f147769287dc1770c251ee46a1f71
AnuragMurti28 <anuragmurti28@gmail.com>
2021-07-07 02:04:21 +0530

    tute67 dir created and server created

--- tute67/index.js
+++ tute67/index.js
@@ -9,9 +9,24 @@ const services = fs.readFileSync('./services.html');
 const contact = fs.readFileSync('./contact.html');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url);
+    url = req.url;
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(homepage);
+    if ((url == '/') || (url = '/home')) {
+        res.end(homepage);
+    } else if (url == '/about') {
+        res.end(about);
+    } else if (url == '/services') {
+        res.end(services);
+    } else if (url == '/contact') {
+        res.end(contact);
+    } else {
+        res.statusCode = 404;
+        res.end("<h1>404 not found</h1>");
+    }
+
 });
 
 server.listen(port, hostname, () => {
